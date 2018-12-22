8d06e7dc278d2d1c82c5be9ecb6f630ae4e2c240
yenchulin <103306003@nccu.edu.tw>
2018-12-22 20:04:10 +0800

    add p1 text

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
     $(window).scroll(function () {
         let scrollTop = $(window).scrollTop() / $(window).height();
+        console.log(scrollTop);
+
         if (scrollTop >= 0.4) {
             $(".pic").moveUp();
-        } else if (scrollTop == 0) {
+            setTimeout(() => {
+                $(".text .p1-2").expandMove();
+            }, 500);
+        } else if (scrollTop === 0) {
             $(".pic").moveBack();
         }
     });
 });
 
 $.fn.moveUp = function () {
-    this.css({ "transform": "translate(0px,-80px)" });
+    this.css({ "transform": "translateY(-80px)" });
 };
 
 $.fn.moveBack = function () {
-    this.css({ "transform": "translate(0px,0px)" });
+    this.css({ "transform": "translateY(0px)" });
+};
+
+$.fn.expandMove = function () {
+    this.css({ "transform": "translate(0px, 0px) scale(1)" });
+};
+
+$.fn.fadeout = function () {
+    this.css({ "opacity": "0" });
 };
